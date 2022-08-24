a8aa09314114d68112e05bc671100b0180a2eb30
JohanstyaN <johan.canon-l@uniminuto.edu.co>
2022-08-23 22:40:43 -0500

    ENTREGUE EL TRABAJO

--- administrar_libro.php
+++ administrar_libro.php
@@ -10,14 +10,14 @@ $libro=new Libro();
 		print_r($_POST);
 		$libro->setNombre($_POST['nombre']);
 	// llama funcion bsertar def en crud
-		$crud-> insertar($libro);
+		$crud->insertar($libro);
 		header('Location: index.php');
 	//
 	}elseif (isset($_POST['actualizar'])) {
 		$libro->setId($_POST['id']);
 		$libro->setNombre($_POST['nombre']);
 		$crud->actualizar($libro);
-		header('Location : index.php');
+		header('Location: index.php');
 	}elseif($_GET['accion']=='e'){
 		$crud->eliminar($_GET['id']);
 		header('Location: index.php');
